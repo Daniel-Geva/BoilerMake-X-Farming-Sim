224bbbd4c5f6638b26fe741ddb92ce149f840fc9
Luderius <dxgeva@gmail.com>
2023-01-20 23:15:09 -0500

    Started Field.java and Interface.java

--- src/Field.java
+++ src/Field.java
@@ -1,2 +1,56 @@
 public class Field {
+    private int[][][] field;
+    private Crop[] crops;
+    private int numFields;
+    private int upgradeValue;
+    //TODO: have an empty crop type
+
+    Field() {
+        this.field = new int[9][5][5];
+        this.crops = new Crop[9];
+        this.numFields = 1;
+        this.upgradeValue = 10;
+    }
+
+    public int harvest() {
+        int total = 0;
+        for (int i = 0; i < numFields; i++) {
+            total += crops[i].value() * crops[i].remaining();
+            crops[i].plant(-1); //set fields empty
+        }
+        return total;
+    }
+
+    public int expandFields(int money) {
+        if (numFields == 9) {
+            return -2;
+        }
+        if (money >= upgradeValue) {
+            money -= upgradeValue;
+            upgradeValue *= 2;
+            numFields++;
+            return money;
+        } else {
+            return -1;
+        }
+    }
+
+    public int plant(int type, int field) {
+        if (field > numFields) {
+            return -1;
+        }
+        if ((type > 5) || (type < 1)) {
+            return -2;
+        }
+        crops[field].plant(type);
+        return type;
+    }
+
+    public void print() {
+        //TODO: print existing fields
+    }
+
+    public int getNumFields() {
+        return numFields;
+    }
 }
--- src/Interface.java
+++ src/Interface.java
@@ -1,2 +1,62 @@
+import java.util.Scanner;
+
 public class Interface {
+    //TODO: figure out how to clear terminal (print many newlines?)
+    private static void planting(Field field) {
+        int numFields = field.getNumFields();
+        System.out.println("You have " + numFields + " fields to plant");
+        System.out.println("You can plant ");
+        //TODO: finish planting method
+    }
+
+    private static void cropInfo() {
+        //TODO: print crop info
+    }
+
+    private static void weatherForecast() {
+        //TODO: forecast the weather
+    }
+
+    private static void upgrade() {
+
+    }
+
+    public static void main(String[] args) {
+        Field field = new Field();
+        int money = 0;
+        Scanner scan = new Scanner(System.in);
+
+        boolean running = true;
+        while (running) {
+            System.out.println("Here is your farm:");
+            field.print();
+            boolean innerLoop = true;
+            while (innerLoop) {
+                System.out.println("Select an action:");
+                System.out.println("1: Plant a field");
+                System.out.println("2: View crop types");
+                System.out.println("3: View weather forecast");
+                System.out.println("4: Upgrade");
+                System.out.println("5: Advance");
+                System.out.println("6: Exit");
+                int response = scan.nextInt();
+                if (response == 1) {
+                    planting(field);
+                } else if (response == 2) {
+                    cropInfo();
+                } else if (response == 3) {
+                    weatherForecast();
+                } else if (response == 4) {
+                    upgrade();
+                } else if (response == 5) {
+                    //TODO: advance time
+                } else if (response == 6) {
+                    innerLoop = false;
+                    running = false;
+                } else {
+                    System.out.println("Invalid selection, please try again");
+                }
+            }
+        }
+    }
 }
