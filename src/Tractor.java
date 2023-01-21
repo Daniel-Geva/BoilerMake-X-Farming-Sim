80497affe90a7422af118d0029829b6363afb34c
krish-ashok <89750507+krish-ashok@users.noreply.github.com>
2023-01-21 14:00:24 -0500

    updated tractor

--- src/Tractor.java
+++ src/Tractor.java
@@ -1,30 +1,66 @@
 public class Tractor {
-    private int level;
-    final int TIME_TO_PLOW_LVL1 = 60;
-    final int TIME_TO_PLOW_LVL2 = 45;
-    final int TIME_TO_PLOW_LVL3 = 35;
-    final int TIME_TO_PLOW_LVL4 = 25;
-    final int TIME_TO_PLOW_LVL5 = 10;
+    private int level = 0; // 1-9
+    private int weatherRes = 0; // weather resistance (scale of 1-4)
+    private int horsepower = 1; // horsepower (scale of 1-4)
+    private String type = ""; // Compact(lvl 1-4), Utility(lvl 5-6), Row Crop(lvl 7-8), 4WD(lvl 9)
 
-    final int ONE_TWO = 50;
-    final int TWO_THREE = 100;
-    final int THREE_FOUR = 150;
-    final int FOUR_FIVE = 300;
+    final int UPGRADE_LVL = 25000;
+    final int UPGRADE_TYPE = 50000;
+
     public Tractor() {
         level = 0;
+        type = "Compact";
+        weatherRes = 0;
+        horsepower = 1;
     }
+
     public Tractor(int l) {
-        level = l;
+        level = 1;
+        adjustStats(level);
+    }
+
+    public void adjustStats(int level) {
+        if((level >= 1) && (level <= 4)) {
+            type = "Compact";
+            weatherRes = 1;
+            horsepower = 20 + (8*level); // 28, 36, 44, 52
+        }
+        else if((level >= 5) && (level <= 6)) {
+            type = "Utility";
+            weatherRes = 2;
+            horsepower = 75 + 75*(level-5); // 75, 150
+        }
+        else if((level >= 7) && (level <= 8)) {
+            type = "Row Crop";
+            weatherRes = 3;
+            horsepower = 275 + 75*(level-7); // 275, 350
+        }
+        else if(level == 9) {
+            type = "4WD";
+            weatherRes = 4;
+            horsepower = 500;
+        }
+        else {
+            type = "ERROR";
+        }
     }
 
     public int plow() {
-        int[] arr = {TIME_TO_PLOW_LVL1, TIME_TO_PLOW_LVL2, TIME_TO_PLOW_LVL3, TIME_TO_PLOW_LVL4, TIME_TO_PLOW_LVL5};
-        return arr[level-1];
+        return 0;
     }
-    public void levelUp(int money) {
-        int[] arr = {ONE_TWO, TWO_THREE, THREE_FOUR, FOUR_FIVE};
-        if(money > arr[level-1]) {
+    public int levelUp(int money) {
+        if (((level == 4) || (level == 6) || (level == 8)) && (money > UPGRADE_TYPE)) {
             level++;
+            adjustStats(level);
+            return level;
+        }
+        else if (((level == 1) || (level == 2) || (level == 3) || (level == 5) || (level == 7)) && (money > UPGRADE_LVL)) {
+            level++;
+            adjustStats(level);
+            return level;
+        }
+        else {
+            return -1;
         }
     }
 }
