2e0c35134f7cd0f4550d31bfa8afe0fa2a467798
Luderius <dxgeva@gmail.com>
2023-01-21 12:43:30 -0500

    Committing Keshav's Crop class because his IntelliJ broke

--- src/Crop.java
+++ src/Crop.java
@@ -6,20 +6,25 @@ public class Crop {
 //Soybeans
 //Okra
 //Wheat
-    //Crop crop = new Crop("Wheat", 3.5, 2, 7, 4, 8, 10);
+    //Crop corn = new Crop("Corn", 3.5, 2, 7, 4, 8, 10);
+    //  System.out.println(corn.getnumber_Name());
 
 
     private String name;
+    private int number_name;
     private double value;
     private int growthStage;
     private int waterLevel;
     private int fertilizerLevel;
     private int health;
     private int harvest;
+    private String[] crop_numbers = new String[]{ "Soil","Corn","Cotton","Cranberries","Rice","Soybeans","Okra","Wheat"};
+
 
 
     public Crop(String name) {
         this.name = name;
+        number_name = setNumberName(name);
         value = 4;
         growthStage = 0;
         waterLevel = 5;
@@ -29,12 +34,15 @@ public class Crop {
     }
     public Crop(String name, double value, int growthStage, int waterLevel, int fertilizerLevel, int health, int harvest) {
         this.name = name;
+        this.number_name = setNumberName(name);
+
         this.value = value;
         this.growthStage = growthStage;
         this.waterLevel = waterLevel;
         this.fertilizerLevel = fertilizerLevel;
         this.health = health;
         this.harvest = harvest;
+
     }
 
 
@@ -72,6 +80,9 @@ public class Crop {
     public int getHealth() {
         return health;
     }
+    public int getnumber_Name() {
+        return number_name;
+    }
 
     public void setHealth(int health) {
         this.health = health;
@@ -83,5 +94,29 @@ public class Crop {
 
     public void setHarvest(int harvest) {
         this.harvest = harvest;
+    }
+    public int setNumberName(String n){
+
+        int index = -1;
+        for (int i=0;i<crop_numbers.length;i++) {
+            if (crop_numbers[i].equals(n)) {
+                index = i;
+                break;
+            }
+
+
+        }
+
+        return index;
+
+
+
+    }
+    public void printTypes(){
+        //	  corn.printTypes();
+        for (int i=1;i<crop_numbers.length;i++) {
+            System.out.println(i + ") " + crop_numbers[i]);
+        }
+
     }
 }
