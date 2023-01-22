a4cf5e9dacdd06d422f906c35fe9ff16c33d1015
nikhilsaxena25 <saxena74@purdue.edu>
2023-01-21 23:18:29 -0500

    fixed the picture

--- src/Weather.java
+++ src/Weather.java
@@ -50,7 +50,7 @@ public class Weather {
     }
 
     public void generateWeather() {
-        int max = types.length - 1;
+        int max = types.length - 2;
         int min = 1;
         int randomIdx = (int) (Math.random() * (max - min + 1) + min);
         status = types[randomIdx];
